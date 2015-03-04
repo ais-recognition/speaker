aeb04093132125a5705ef01d0d4b62c52f51f0e0
to0 <yuanancai@gmail.com>
2015-03-04 02:15:55 -0500

    add mqtt lib, add message sound

---
+++ SpeakerClient/SpeakerClient-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SpeakerClient-Bridging-Header.h
+//  SpeakerClient
+//
+//  Created by to0 on 3/4/15.
+//  Copyright (c) 2015 to0. All rights reserved.
+//
+
+#import "SwiftMQTT.h"
---
+++ SwiftMQTT/SwiftMQTT.h
@@ -0,0 +1,15 @@
+//
+//  SwiftMQTT.h
+//  SpeakerClient
+//
+//  Created by to0 on 3/4/15.
+//  Copyright (c) 2015 to0. All rights reserved.
+//
+
+#ifndef SpeakerClient_SwiftMQTT_h
+#define SpeakerClient_SwiftMQTT_h
+
+    #import "MSWeakTimer.h"
+    #import "GCDAsyncSocket.h"
+
+#endif
