a985d6b341733791eecc5533b851e6dd151037da
to0 <yuanancai@gmail.com>
2015-03-18 13:49:59 +0800

    fatalError

--- SpeakerClient/View/MessageInputAccessoryView.swift
+++ SpeakerClient/View/MessageInputAccessoryView.swift
@@ -65,7 +65,9 @@ class MessageInputAccessoryView: UIToolbar, UITextViewDelegate {
     }
     
     required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+//        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+
     }
     
     func textViewDidChange(textView: UITextView) {
